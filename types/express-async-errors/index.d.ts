b8dae7437b6bc449506a24ad75c6b5aacaca89c9
Stephen Melvin <steve.melvin@dtn.com>
2022-03-28 12:58:37 -0400

    Added @types/express-async-errors definition

---
+++ types/express-async-errors/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for express-async-errors 3.1.1
+// Project: https://github.com/davidbanham/express-async-errors
+// Definitions by: Stephen Melvin <https://github.com/sbmelvin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'express-async-errors' {
+  export = Object;
+}
